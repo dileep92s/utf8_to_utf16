c5f172a7129f29b834984d4e23f1f20d16312fcc
dileep92s <dileep92s@gmail.com>
2022-09-26 16:33:39 +0530

    conv codepoint to utf16

--- utfconv.c
+++ utfconv.c
@@ -6,14 +6,14 @@
 #define MASK_FIRST_BYTE 0xC0
 #define MASK_NEXT_BYTES 0x80
 
-bool hasUTF8(const uint8_t *data, uint16_t len)
+bool hasUTF8(const uint8_t *utf8_buff, uint16_t utf8_buff_len)
 {
     bool ret = false;
-    if (data != NULL)
+    if (utf8_buff != NULL)
     {
-        for (int i = 0; i < len; i++)
+        for (int i = 0; i < utf8_buff_len; i++)
         {
-            if ((data[i] & MASK_FIRST_BYTE) == MASK_FIRST_BYTE)
+            if ((utf8_buff[i] & MASK_FIRST_BYTE) == MASK_FIRST_BYTE)
             {
                 ret = true;
                 break;
@@ -23,19 +23,18 @@ bool hasUTF8(const uint8_t *data, uint16_t len)
     return ret;
 }
 
-const uint8_t *getUTF16(const uint8_t *data, uint16_t len, uint16_t *utf16)
+const uint8_t *getCodeFromUTF8(const uint8_t *utf8_buff, uint16_t utf8_buff_len, uint32_t *code)
 {
     uint32_t temp = 0;
     int8_t size = 0;
-    const int8_t *ret = NULL;
+    const int8_t *retAddr = NULL;
 
-    if ((data != NULL) && (utf16 != NULL) && (len != 0))
+    if ((utf8_buff != NULL) && (code != NULL) && (utf8_buff_len != 0))
     {
-        *utf16 = 0;
-        for (uint8_t i = 0; i < len; i++)
+        for (uint8_t i = 0; i < utf8_buff_len; i++)
         {
             uint8_t mask = MASK_FIRST_BYTE;
-            while ((data[i] & mask) == mask)
+            while ((utf8_buff[i] & mask) == mask)
             {
                 mask = (int8_t)mask >> 1;
                 size++;
@@ -43,54 +42,66 @@ const uint8_t *getUTF16(const uint8_t *data, uint16_t len, uint16_t *utf16)
 
             if (mask != MASK_FIRST_BYTE)
             {
-                temp = (data[i] & ~mask);
+                temp = (utf8_buff[i] & ~mask);
             }
-            else if ((data[i] & MASK_NEXT_BYTES) == MASK_NEXT_BYTES)
+            else if ((utf8_buff[i] & MASK_NEXT_BYTES) == MASK_NEXT_BYTES)
             {
-                temp = (temp << 6) | (data[i] & 0x3F);
+                temp = (temp << 6) | (utf8_buff[i] & 0x3F);
                 size--;
             }
             else
             {
-                temp = data[i];
+                temp = utf8_buff[i];
                 size = 0;
             }
 
             if (size == 0)
             {
-                printf("%x %x %d %x\n", data[i], mask, size, temp);
-                ret = (data + i + 1);
+                // printf("%x %x %d %x\n", utf8_buff[i], mask, size, temp);
+                *code = temp;
+                retAddr = (utf8_buff + i + 1);
                 break;
             }
         }
     }
-    return ret;
+    return retAddr;
 }
 
-uint16_t convUTF16(const uint8_t *utf8, uint16_t utf8_len, uint16_t *utf16)
+void convUTF16(const uint8_t *utf8_buff, uint16_t utf8_buff_len, uint16_t *utf16_buff)
 {
-    uint16_t utf16_len = 0;
-    uint16_t utf16_char = 0;
-    const uint8_t *begin = utf8;
-    const uint8_t *end = &utf8[utf8_len];
+    const uint8_t *begin = utf8_buff;
+    const uint8_t *end = &utf8_buff[utf8_buff_len];
+    uint16_t idx = 0;
     while (begin != end)
     {
-        uint16_t len = end - begin;
-        begin = getUTF16(begin, len, &utf16_char);
+        uint32_t code = 0;
+        begin = getCodeFromUTF8(begin, (end - begin), &code);
         if (begin == NULL)
         {
-            printf("parse error\n");
+            printf("error while getting code point!\n");
             break;
         }
+        else
+        {
+            if (code > 0xFFFF)
+            {
+                code = code - 0x10000;
+                utf16_buff[idx++] = (code >> 10) + 0xD800;
+                utf16_buff[idx++] = (code & 0x3FF) + 0xDC00;
+            }
+            else
+            {
+                utf16_buff[idx++] = (uint16_t)code;
+            }
+        }
     }
-    return utf16_len;
 }
 
 int main()
 {
-    // uint8_t data[] = {0xe2, 0x82, 0xac};
-    // uint8_t data[] = {0xF0, 0x90, 0x8D, 0x88};
-    uint8_t utf8[] = {0xF0, 0x90, 0x8D, 0x88, 0xe2, 0x82, 0xac, 0x65};
+    // bYҁ짼幎娳򰇑9ޫފؓ󀀪䝶1�򊆈镶䉆𡹑োÉƯ󲎱ς؅趏e܀撄𴟇懗
+    uint8_t utf8[] = {0x62, 0x59, 0xd2, 0x81, 0xec, 0xa7, 0xbc, 0xe5, 0xb9, 0x8e, 0xe5, 0xa8, 0xb3, 0xf2, 0xb0, 0x87, 0x91, 0xee, 0x82, 0x8a, 0x39, 0xde, 0xab, 0xde, 0x8a, 0xd8, 0x93, 0xf3, 0x80, 0x80, 0xaa, 0xe4, 0x9d, 0xb6, 0x31, 0xef, 0xbf, 0xbd, 0xf2, 0x8a, 0x86, 0x88, 0xe9, 0x95, 0xb6, 0xe4, 0x89, 0x86, 0xf0, 0xa1, 0xb9, 0x91, 0xe0, 0xa7, 0x8b, 0xc3, 0x89, 0xc6, 0xaf, 0xf3, 0xb2, 0x8e, 0xb1, 0xcf, 0x82, 0xd8, 0x85, 0xe8, 0xb6, 0x8f, 0x65, 0xdc, 0x80, 0xe6, 0x92, 0x84, 0xf0, 0xb4, 0x9f, 0x87, 0xe6, 0x87, 0x97};
+    // expected : ['0x0', '0x62', '0x0', '0x59', '0x4', '0x81', '0xc9', '0xfc', '0x5e', '0x4e', '0x5a', '0x33', '0xda', '0x80', '0xdd', '0xd1', '0xe0', '0x8a', '0x0', '0x39', '0x7', '0xab', '0x7', '0x8a', '0x6', '0x13', '0xda', '0xc0', '0xdc', '0x2a', '0x47', '0x76', '0x0', '0x31', '0xff', '0xfd', '0xd9', '0xe8', '0xdd', '0x88', '0x95', '0x76', '0x42', '0x46', '0xd8', '0x47', '0xde', '0x51', '0x9', '0xcb', '0x0', '0xc9', '0x1', '0xaf', '0xdb', '0x88', '0xdf', '0xb1', '0x3', '0xc2', '0x6', '0x5', '0x8d', '0x8f', '0x0', '0x65', '0x7', '0x0', '0x64', '0x84', '0xd8', '0x91', '0xdf', '0xc7', '0x61', '0xd7']
     uint16_t utf16[16] = {0x00};
 
     if (hasUTF8(utf8, sizeof(utf8)))
@@ -98,7 +109,11 @@ int main()
         convUTF16(utf8, sizeof(utf8), utf16);
     }
 
-    // convUTF16(utf8, sizeof(utf8), NULL);
+    for (int i = 0; i < sizeof(utf16) / sizeof(utf16[0]); i++)
+    {
+        printf("%x ", utf16[i]);
+    }
+    printf("\n");
 
     return 0;
 }
